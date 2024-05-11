6ea8ae53817d69ba08a9108d36b72188adb6700d
francisco risso <frisso2001@gmail.com>
2024-05-11 13:37:28 -0300

    Add Profile type to backend/frontend

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5,3 +5,12 @@ export type User = {
 export type Ranking = {
   entries: Array<{ user: User; points: number }>;
 };
+
+export type Profile = {
+  user: User;
+  metadata: {
+    totalHours: number;
+    mirrorHours: number;
+    triangleHours: number;
+  };
+};
